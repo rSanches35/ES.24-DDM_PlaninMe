3b418c12a2c9638980d9e8a976e1bdfd92aa6504
rSanches35 <rafaelsanches.s35@gmail.com>
2024-09-12 22:17:55 -0300

    Correção de Diretórios

--- planin_me/test/app/base/sqlite/script_test.dart
+++ planin_me/test/app/base/sqlite/script_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-import 'package:planin_me/app/base/script.dart';
+import 'package:planin_me/app/base/sqlite/script.dart';
 
 void main() {
   setUpAll(() {
