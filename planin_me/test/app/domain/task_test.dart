c7b6068ab74d2dd7b3c50048b867e9cf4e71cc94
rSanches35 <rafaelsanches.s35@gmail.com>
2024-10-03 20:25:39 -0300

    Ajustes de Task_Test

--- planin_me/test/app/domain/task_test.dart
+++ planin_me/test/app/domain/task_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:planin_me/app/domain/interface/idao_task.dart';
@@ -7,22 +6,20 @@ import 'package:planin_me/app/domain/times_a_day.dart';
 import 'package:planin_me/app/domain/task.dart';
 
 void main() {
-
   //Attributes
   late Task task;
   late TimesADay times_a_day;
   late DTOTask dto;
   late DAOTaskFake dao;
 
-  group(['GROUP_Description'], () { 
-
-    setUp(() { dao = DAOTaskFake();});
+  group(['GROUP_Description'], () {
+    setUp(() {
+      dao = DAOTaskFake();
+    });
 
     test('TEST_Description', () {
 
-      times_a_day = TimesADay();
-
-      dto = DTOTask(name: "Test", times_a_day: times_a_day, priority: 2);
+      dto = DTOTask(name: "Test", priority: 2);
       task = Task(dto: dto, dao: dao);
 
       expect(() => task.save(dto), returnsNormally);
@@ -32,7 +29,8 @@ void main() {
 
 //DAOFake
 class DAOTaskFake implements IDAOTask {
-  
   @override
-  DTOTask save(DTOTask dto) {return dto;}
+  DTOTask save(DTOTask dto) {
+    return dto;
+  }
 }
