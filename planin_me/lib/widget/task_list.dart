ad0144f9338d861dd8f38114d54b11207294a724
rSanches35 <rafaelsanches.s35@gmail.com>
2024-10-03 22:25:31 -0300

    Adição de Navigators

--- planin_me/lib/widget/task_list.dart
+++ planin_me/lib/widget/task_list.dart
@@ -1,23 +1,20 @@
+import 'dart:math';
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:planin_me/widget/routes.dart';
 import 'package:planin_me/app/domain/dto/task_dto.dart';
+import 'package:planin_me/widget/task_details.dart';
+import 'package:planin_me/widget/task_form.dart';
 
 class TaskList extends StatelessWidget {
   const TaskList({Key? key}) : super(key: key);
 
   Future<List<DTOTask>> consult() async {
     return [
-      DTOTask(
-          name: 'Wake Up',
-          priority: 3,
-          isActive: true),
-      DTOTask(
-          name: 'Feed Neko',
-          priority: 1,
-          isActive: true),
-      DTOTask(
-          name: 'Do Laundry',
-          priority: 2,
-          isActive: false),
+      DTOTask(name: 'Wake Up', priority: 3, isActive: true),
+      DTOTask(name: 'Feed Neko', priority: 1, isActive: true),
+      DTOTask(name: 'Do Laundry', priority: 2, isActive: false),
     ];
   }
 
@@ -26,14 +23,15 @@ class TaskList extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Task List'),
+        backgroundColor: Colors.cyan[300],
       ),
       body: FutureBuilder(
         future: consult(),
-        builder: (BuildContext context, AsyncSnapshot<List<DTOTask>> snapshot) {
+        builder:
+          (BuildContext context, AsyncSnapshot<List<DTOTask>> snapshot) {
           var dataList = snapshot.data;
-          if (dataList == null) {
-            return Text('Data not Found');
-          } else {
+          if (dataList == null) { return Text('Data not Found');}
+          else {
             List<DTOTask> list = dataList;
             return ListView.builder(
               itemCount: list.length,
@@ -43,11 +41,22 @@ class TaskList extends StatelessWidget {
                   leading: Icon(Icons.insert_invitation_rounded),
                   title: Text(task.name),
                   subtitle: Text('Priority: ${task.priority}'),
+                  onTap: () => Navigator.push( context,
+                    MaterialPageRoute(builder: (context) => const TaskDetails()),
+                  ),
                 );
               },
             );
           }
         },
+      ),
+      floatingActionButton: IconButton(
+        icon: Icon(Icons.add),
+        color: Colors.cyan[700],
+        iconSize: 30,
+        onPressed: () => Navigator.push( context,
+          MaterialPageRoute(builder: (context) => const TaskForm()),
+        ),
       ),
     );
   }
